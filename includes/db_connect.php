93b2805b2baa85f6c3ead928eb59e8a821c3f90b
GEORGE2102 <georgebandha350@gmail.com>
2025-04-14 18:09:34 +0200

    connection commint

---
+++ includes/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sensor_readings"; // replace with your DB name
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
